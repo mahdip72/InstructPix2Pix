3704bae9516e86a20aee06c84676e2966ad3d784
mahdi <mahdipoormirzaie@gmail.com>
2024-05-23 16:08:53 -0500

    ref: remove uv installer form install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-pip install uv
 conda install --yes pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
 conda install --yes xformers -c xformers
 conda install --yes h5py
 conda install --yes matplotlib
 conda install --yes pandas
-uv pip install accelerate
-uv pip install transformers
-uv pip install diffusers
-uv pip install datasets
-uv pip install 'git+https://github.com/katsura-jp/pytorch-cosine-annealing-with-warmup'
-uv pip install tensorboard
-uv pip install python-box
-uv pip install peft
-uv pip install -U scikit-learn
+pip install accelerate
+pip install transformers
+pip install diffusers
+pip install datasets
+pip install 'git+https://github.com/katsura-jp/pytorch-cosine-annealing-with-warmup'
+pip install tensorboard
+pip install python-box
+pip install peft
+pip install -U scikit-learn
